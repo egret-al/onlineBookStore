3c835f0776479e1b30f93584d5341a2803f7d9c9
admin <2243756824@qq.com>
2020-09-09 20:45:18 +0800

    搭建客户端环境

---
+++ online-bookstore-client/online-bookstore-client/vue.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+
+  //webpack相关配置
+  devServer: {
+    open: true,
+    port: 8000
+  },
+  css: {
+    loaderOptions: {
+      stylus: {
+        'resolve url': true,
+        'import': [
+          './src/theme'
+        ]
+      }
+    }
+  },
+  pluginOptions: {
+    'cube-ui': {
+      postCompile: true,
+      theme: true
+    }
+  }
+}
