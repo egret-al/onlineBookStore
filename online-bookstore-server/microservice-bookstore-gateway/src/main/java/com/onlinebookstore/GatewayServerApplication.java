8fcd242252ef4e0c71c0c3ddf680f0fb6461ddb4
admin <2243756824@qq.com>
2020-09-10 17:13:41 +0800

    搭建后台分布式微服务

---
+++ online-bookstore-server/microservice-bookstore-gateway/src/main/java/com/onlinebookstore/GatewayServerApplication.java
@@ -0,0 +1,19 @@
+package com.onlinebookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author rkc
+ * @version 1.0
+ * @date 2020/9/10 16:57
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServerApplication.class, args);
+    }
+}
